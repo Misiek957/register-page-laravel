d3e2db46e2ad7f1fc2b7d9ff291203420272c8e0
Misiek957 <misiekleaim@gmail.com>
2021-12-11 02:41:07 +0000

    Added post seeding and factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use app\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 2,
+            'title' => $this->faker->sentence(3),
+            'excerpt' => $this->faker->sentence(10),
+            'body' =>  $this->faker->sentence(100),
+        ];
+    }
+}
