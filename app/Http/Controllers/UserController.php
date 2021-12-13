8a31215f83e3b320c6ad6a03d4dceb8727a5bd47
Misiek957 <misiekleaim@gmail.com>
2021-12-13 20:29:32 +0000

    Fixed update of user details page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,17 +13,25 @@ class UserController extends Controller
     }
 
     public function update(Request $request, User $user){
-
-        $request->validate([
+        $attributes = $request->validate([
             'name'=>'max:255',
             'username'=>'max:255|min:3', // how to ensure no duplicates
             'email'=>'email|max:255',
             'password' => 'min:6|max:255|same:repeat_password',
             'repeat_password' => 'max:255|min:7',
         ]); 
-        $user->update($request->all());    
+        
+        //$user->update($request->all());    
         //User::update($attributes);
         // User::update($attributes); // Send the successfuly requested attributed into the user creation template
+        $user_id = Auth::user()->id;
+        $user = User::find($user_id);
+        $user->name = $attributes["name"];
+        $user->username = $attributes["username"];
+        $user->password = $attributes["password"]; // Hashing already done at setPassword
+        $user->save();
+
+
         session()->flash('success','Account has been updated');
         return redirect('/account');
     }
